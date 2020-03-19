0d364ca2af86c52d6d8cbafb69a3082c7c23887e
lijiliang <ljlprismo@gmail.com>
2020-03-19 18:32:11 +0800

    啊

--- new/new.js
+++ new/new.js
@@ -14,12 +14,14 @@ Otaku.prototype.sayYourName = function () {
 function objectFactory() {
     var obj = new Object()
     Constructor = [].shift.call(arguments)
+
     obj.__proto__ = Constructor.prototype
-    
+
     // 如果构造函数返回了一个对象，在实例 person 中只能访问返回的对象中的属性。
     // 如果构造函数返回基本类型的值，则按没有返回值处理
 
     var ret = Constructor.apply(obj, arguments);
-    return typeof ret === 'object' ? ret : obj
+    return typeof ret === 'object' ? ret || obj : obj
 }
 
+var o = objectFactory(Otaku, 'jake', 12)
